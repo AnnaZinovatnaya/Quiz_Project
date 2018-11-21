d25842c0fcd4c71835a8f3a9ce8fc950a5ca67ee
HZinovatna <Hzinovatna@luxoft.com>
2018-11-21 11:12:41 +0200

    Update admin page

--- quizproject/testing/admin.py
+++ quizproject/testing/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Test, Question, Answer
 
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['test', 'name']
+    inlines = [AnswerInline]
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
 admin.site.register(Test)
-admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(Question, QuestionAdmin)
